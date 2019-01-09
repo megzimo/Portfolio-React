d07056371ba6d5639b8ff5dbd81bf878dcd5b0dc
megzimo <40956115+megzimo@users.noreply.github.com>
2019-01-09 10:26:42 -0600

    fix href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 export default class Footer extends Component {
   render() {
     let resumeData = this.props.resumeData;
     return (
-      <footer id="connect">
-      <div className="row">
-      <h2>
-        connect with me
-      </h2>
-        <div className="twelve columns">
-          <ul className="social-links">
-            {
-              resumeData.socialLinks && resumeData.socialLinks.map((item)=>{
-                return(
-                  <li>
-                    <a href={item.url}>
-                    <i className={item.className} />
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          
-        </div>
-        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
-      </div>
-    </footer>
+      <section id="contact">
+        <footer>
+          <div className="row">
+            <h2>connect with me</h2>
+            <div className="twelve columns">
+              <ul className="social-links">
+                {resumeData.socialLinks &&
+                  resumeData.socialLinks.map(item => {
+                    return (
+                      <li>
+                        <a href={item.url}>
+                          <i className={item.className} />
+                        </a>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+            <div id="go-top">
+              <a className="smoothscroll" title="Back to Top" href="#home">
+                <i className="icon-up-open" />
+              </a>
+            </div>
+          </div>
+        </footer>
+      </section>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ export default class Header extends Component {
                <li><a className="smoothscroll atb-modern-b" data-text="about" href="#about"><span>a</span><span>b</span><span>o</span><span>u</span><span>t</span></a></li>
                <li><a className="smoothscroll atb-modern-b" data-text="resume" href="#resume"><span>r</span><span>e</span><span>s</span><span>u</span><span>m</span><span>e</span></a></li>
                <li><a className="smoothscroll atb-modern-b" data-text="works" href="#portfolio"><span>w</span><span>o</span><span>r</span><span>k</span><span>s</span></a></li>
-               <li><a className="smoothscroll atb-modern-b" data-text="contact" href="#footer"><span>c</span><span>o</span><span>n</span><span>t</span><span>a</span><span>c</span><span>t</span></a></li>
+               <li><a className="smoothscroll atb-modern-b" data-text="contact" href="#contact"><span>c</span><span>o</span><span>n</span><span>t</span><span>a</span><span>c</span><span>t</span></a></li>
             </ul>
          </nav>
 
